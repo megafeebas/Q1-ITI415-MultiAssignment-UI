b844fcd6838401163a656870a917569b11656eea
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-05-07 18:54:58 -0400

    Switched Trigger

--- Assets/Scripts/sCollection.cs
+++ Assets/Scripts/sCollection.cs
@@ -7,22 +7,26 @@ public class sCollection : MonoBehaviour
 {
     // Start is called before the first frame update
     private int objects = 4;
+    public Text countText;
+
     void Awake()
     {
+        countText.text = "Count: " + objects.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Ore")
         {
             objects--;
-            gameObject.GetComponent<Renderer>().enabled = false;
+            countText.text = "Count: " + objects.ToString();
         }
-
+        Destroy(other.gameObject);
     }
 
 }
